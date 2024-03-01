c33e3faf294db25bfe4d19a92909e1db68e5eee0
zxymax <497084653@qq.com>
2024-03-01 16:09:07 +0800

    Hello C++ World

---
+++ 01.C++HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << endl;
+
+    // without using namespace std;
+    std::cout << "Hello C++" << std::endl;
+
+    return 0;
+}
