9e19223e4b6302f03e36aa12d664f738348e7dfc
zxymax <497084653@qq.com>
2024-03-03 14:54:07 +0800

    Hello C++ World

--- 01.C++HelloWorld.cpp
+++ 01.C++HelloWorld.cpp
@@ -9,5 +9,21 @@ int main()
     // without using namespace std;
     std::cout << "Hello C++" << std::endl;
 
+    // letter
+    std::cout << "Dear Self,            \n";
+    std::cout << "                      \n";
+    std::cout << "I will build a VR app.\n";
+    std::cout << "                      \n";
+    std::cout << "                -Sonny\n";
+
+    // pattern
+    std::cout << "       1\n";
+    std::cout << "     2 3\n";
+    std::cout << "   4 5 6\n";
+    std::cout << "7 8 9 10\n";
+
+    // spell
+    std::cout << "Expecto Patronum\n";
+
     return 0;
 }
